e16d53663a9656c25e801b21afcd718430745235
Gurdand <zmei83666@gmail.com>
2019-02-27 19:39:13 +0300

    added docs for git.pages

---
+++ docs/precache-manifest.5b8fb0a3ea89ca657b9a498c61413922.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "2bf94c6e499a961a608dd94a5056e08d",
+    "url": "/static/media/flag.2bf94c6e.png"
+  },
+  {
+    "revision": "7bd389ae25e574942be6341e5511c154",
+    "url": "/static/media/1559584-guild-wars-2-wallpaper.7bd389ae.jpg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "7465d9185a8d761a9f6b",
+    "url": "/static/js/main.7465d918.chunk.js"
+  },
+  {
+    "revision": "b3e7dc87b2a27212fafa",
+    "url": "/static/js/1.b3e7dc87.chunk.js"
+  },
+  {
+    "revision": "7465d9185a8d761a9f6b",
+    "url": "/static/css/main.6f2771ae.chunk.css"
+  },
+  {
+    "revision": "6f4a68c06b6bbcb67bbc24c474a42d02",
+    "url": "/index.html"
+  }
+];
